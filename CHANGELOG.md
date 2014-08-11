9c9ec68a056ca4f62a6f58264ef9d66861daa6ac
Remo Mueller <remosm@gmail.com>
2014-08-11 14:47:28 -0400

    Added ability to navigate through EDF using right and left arrows and turbolinks

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,4 @@
 ### Enhancements
 - Added basic testing framework for rack app
 - Added ability to load an EDF signal into an HTML canvas
+- Added ability to navigate through EDF using right and left arrows and turbolinks
--- public/assets/application.js
+++ public/assets/application.js
@@ -1,6 +1,12 @@
-function point(x,y) {
-  window.$ctx.fillStyle = "#333333";
-  window.$ctx.fillRect(x,c.height-y,2,2);
+function point(x,y,width,color) {
+  if (width == null) {
+    width = 2;
+  }
+  if (color == null) {
+    color = "#333333";
+  }
+  window.$ctx.fillStyle=color;
+  window.$ctx.fillRect(x,window.$canvas.height-y,x+width,window.$canvas.height-y+width);
 }
 
 function line(x,y,x2,y2,width,color) {
@@ -40,21 +46,18 @@ function drawSignal(array,x_offset,y_offset,label,samples_per_data_record) {
     x_offset = 70;
   }
 
-  line_length = array.length*10
-  offset_line(0,0,line_length,0,1,"#ededed",x_offset,y_offset);
   offset_label(-100,0,label,x_offset,y_offset);
 
-  zoom_level = 10;
+
+  var signal_canvas_width = window.$canvas.width - x_offset;
+  var zoom_level = signal_canvas_width / $('#myCanvas').data('samples-per-page');
+
+  offset_line(0,0,signal_canvas_width,0,1,"#ededed",x_offset,y_offset);
 
   var magnitude_x = zoom_level*1/samples_per_data_record;
   var magnitude_y = 1;
   for (var i=0, l=array.length; i < l; i+=1) {
-    if( i == 3 ){
-      // break;
-    }
-    if( i < l - 1 ){
-      // console.log(array[i]*10 + " " + array[i+1]*10);
-
+    if ( i < l - 1 ) {
       offset_line(i*magnitude_x, array[i]*magnitude_y,(i+1)*magnitude_x, array[i+1]*magnitude_y,1,null,x_offset,y_offset);
     }
   }
@@ -63,16 +66,37 @@ function drawSignal(array,x_offset,y_offset,label,samples_per_data_record) {
 function ready () {
   window.$canvas = $("#myCanvas")[0];
   window.$ctx = window.$canvas.getContext("2d");
-  array = $("#edf-signals").data('array');
-  array2 = $("#edf-signals").data('array2');
+
+  // line(window.$canvas.width, 0, window.$canvas.width, window.$canvas.height, 2, '#FF0000');
 
   var canvas_height = window.$canvas.height;
   var signal_height = 260;
   $("[data-object='signal-data']").each(function(index){
     drawSignal($(this).data('array'),100,canvas_height-((signal_height / 2) + index*signal_height),$(this).data('label'),parseInt($(this).data('samples-per-data-record')));
+    $(this).removeAttr('data-array');
   });
 }
 
 $(document).ready(function() {
   ready();
+});
+$(document).on('page:load', ready)
+$(document).on("page:change", function() {
+  window.$prevPageYOffset = window.pageYOffset;
+  window.$prevPageXOffset = window.pageXOffset;
+});
+$(document).on("page:load", function() {
+  window.scrollTo(window.$prevPageXOffset, window.$prevPageYOffset);
+});
+
+
+$(document).keydown( function(e) {
+  if(e.which == 37 && !$("input, textarea, select, a").is(":focus")){
+    $("#retreat")[0].click();
+    return false;
+  }
+  if(e.which == 39 && !$("input, textarea, select, a").is(":focus")){
+    $("#advance")[0].click();
+    return false;
+  }
 });
