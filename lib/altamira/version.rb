b9fd6e2469b7918b86f20cc52eee3b6241b95f00
Remo Mueller <remosm@gmail.com>
2017-01-05 15:39:44 -0500

    Version bump to 0.3.0.beta2

--- lib/altamira/version.rb
+++ lib/altamira/version.rb
@@ -5,7 +5,7 @@ module Altamira
     MAJOR = 0
     MINOR = 3
     TINY = 0
-    BUILD = 'beta1' # 'pre', 'rc', 'rc2', nil
+    BUILD = 'beta2' # 'pre', 'rc', 'rc2', nil
 
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
   end
