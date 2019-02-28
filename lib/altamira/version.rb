2550ba0c5eba1be7b887f62d853bde885872e678
Remo Mueller <remosm@gmail.com>
2019-02-28 11:41:56 -0500

    Use double quotes

--- lib/altamira/version.rb
+++ lib/altamira/version.rb
@@ -5,8 +5,8 @@ module Altamira
     MAJOR = 0
     MINOR = 4
     TINY = 0
-    BUILD = 'pre' # 'pre', 'rc', 'rc2', nil
+    BUILD = "pre" # "pre", "beta1", "beta2", "rc", "rc2", nil
 
-    STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
+    STRING = [MAJOR, MINOR, TINY, BUILD].compact.join(".")
   end
 end
