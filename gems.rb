4eb3b09bf5d509cf48b2d74b110d4c1b6e0c60c1
Remo Mueller <remosm@gmail.com>
2019-02-28 11:56:13 -0500

    Improve readability

--- gems.rb
+++ gems.rb
@@ -1,18 +1,20 @@
 # frozen_string_literal: true
 
+# rubocop:disable Layout/ExtraSpacing
 source "https://rubygems.org"
 
-gem "edfize", "~> 0.4.0"
-gem "rack",   "~> 2.0.1"
-gem "rake",   "~> 12.0.0"
-gem "xml-simple", "~> 1.1.5"
+gem "edfize",            "~> 0.4.0"
+gem "rack",              "~> 2.0.1"
+gem "rake",              "~> 12.0.0"
+gem "xml-simple",        "~> 1.1.5"
 
 gem "coffee-script"
-gem "sprockets", "~> 3.7.0"
+gem "sprockets",         "~> 3.7.0"
 gem "turbolinks-source", "~> 5.0.0"
 
 # Testing
 group :test do
   gem "minitest"
-  gem "simplecov", "~> 0.12.0", require: false
+  gem "simplecov",       "~> 0.16.1", require: false
 end
+# rubocop:enable Layout/ExtraSpacing
