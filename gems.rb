fa59e79b4f96ddead166528ea420da043b34ff3a
Remo Mueller <remosm@gmail.com>
2019-02-28 11:54:14 -0500

    Prefer use of double quotes

--- gems.rb
+++ gems.rb
@@ -1,18 +1,18 @@
 # frozen_string_literal: true
 
-source 'https://rubygems.org'
+source "https://rubygems.org"
 
-gem 'edfize', '~> 0.4.0'
-gem 'rack',   '~> 2.0.1'
-gem 'rake',   '~> 12.0.0'
-gem 'xml-simple', '~> 1.1.5'
+gem "edfize", "~> 0.4.0"
+gem "rack",   "~> 2.0.1"
+gem "rake",   "~> 12.0.0"
+gem "xml-simple", "~> 1.1.5"
 
-gem 'coffee-script'
-gem 'sprockets', '~> 3.7.0'
-gem 'turbolinks-source', '~> 5.0.0'
+gem "coffee-script"
+gem "sprockets", "~> 3.7.0"
+gem "turbolinks-source", "~> 5.0.0"
 
 # Testing
 group :test do
-  gem 'minitest'
-  gem 'simplecov', '~> 0.12.0', require: false
+  gem "minitest"
+  gem "simplecov", "~> 0.12.0", require: false
 end
