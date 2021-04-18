02b3a62fe77154ede9f2bcb1dd3efc63df606c6a
Remo Mueller <remosm@gmail.com>
2021-04-18 14:07:18 -0400

    Version bump to 5.0.0.pre

--- lib/altamira/version.rb
+++ lib/altamira/version.rb
@@ -2,10 +2,10 @@
 
 module Altamira
   module VERSION #:nodoc:
-    MAJOR = 4
+    MAJOR = 5
     MINOR = 0
     TINY = 0
-    BUILD = nil # "pre", "beta1", "beta2", "rc", "rc2", nil
+    BUILD = "pre" # "pre", "beta1", "beta2", "rc", "rc2", nil
 
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join(".")
   end
